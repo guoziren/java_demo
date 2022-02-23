f9a4adb91b6204fe8bd3aeb10f18ec85e04950bd
lycguo <lycguo@tencent.com>
2022-02-23 12:45:01 +0800

    使用InitializingBean、BeanFactoryPostProcessor、@PostConstruct

---
+++ spring_inject_bean/src/main/java/com/ustc/demo/inject/MyBeanFactoryPostProcessor.java
@@ -0,0 +1,21 @@
+package com.ustc.demo.inject;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description:
+ * @Date: 2022/2/23
+ */
+@Slf4j
+@Component
+public class MyBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        log.info("MyBeanFactoryPostProcessor");
+    }
+}
--- spring_inject_bean/src/main/java/com/ustc/demo/inject/bean/Apple.java
+++ spring_inject_bean/src/main/java/com/ustc/demo/inject/bean/Apple.java
@@ -1,5 +1,8 @@
 package com.ustc.demo.inject.bean;
 
+import javax.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 /**
@@ -7,7 +10,13 @@ import org.springframework.stereotype.Component;
  * @Date: 2022/2/22
  */
 @Component("apple")
-public class Apple extends Fruit {
+@Slf4j
+public class Apple extends Fruit implements InitializingBean {
+
+    public Apple() {
+        super();
+        log.info("Apple 构造函数...");
+    }
 
     @Override
     public String getName() {
@@ -17,5 +26,17 @@ public class Apple extends Fruit {
     @Override
     public int getOrder() {
         return 1;
+    }
+
+
+
+    @PostConstruct
+    public void testPostConstruct () {
+        log.info("Apple PostConstruct");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.info("Apple afterPropertiesSet");
     }
 }
